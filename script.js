0f5e3e7231c4d9b405a5a273e5cf913da554eea1
Nusrat-Islam345 <nusratnisi345@gmail.com>
2024-11-16 15:50:29 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    document.getElementById("name").innerHTML = "MIM";
+}
+
+
+function change(){
+    document.getElementById('demo').innerHTML = "Tonni";
+}
+
+
+
+function myimage() {
+    document.getElementById('myimage').src='image/Nusrat.jpg'
+   }
+ 
+ function image(){
+     document.getElementById('myimage').src='image/samantha.png'
+ }
+
+
+
+
+function fontChange(){
+    document.getElementById('font').style.fontSize="35px";
+}
+
+
+function friend(){
+    document.getElementById('friend-none').style.display="none"
+}
+
+
+function showtext(){ document.getElementById('show').style.display='block'
+}
